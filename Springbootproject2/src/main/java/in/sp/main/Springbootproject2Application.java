1c2f4129d6845ce8ffdf056803cf7f75230f2e7c
DivyanshuSingh43 <singhdivyanshu0408@gmail.com>
2026-02-01 00:49:35 +0530

    Add files via upload

---
+++ Springbootproject2/src/main/java/in/sp/main/Springbootproject2Application.java
@@ -0,0 +1,14 @@
+package in.sp.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootproject2Application {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Springbootproject2Application.class, args);
+	}
+
+}
