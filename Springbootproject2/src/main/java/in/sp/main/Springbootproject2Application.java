f212ce5d4abea71e7a0070d4bf8a1d5690f24497
DivyanshuSingh43 <singhdivyanshu0408@gmail.com>
2026-02-05 23:34:08 +0530

    Update Springbootproject2Application.java

--- Springbootproject2/src/main/java/in/sp/main/Springbootproject2Application.java
+++ Springbootproject2/src/main/java/in/sp/main/Springbootproject2Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Springbootproject2Application {
-
+  
 	public static void main(String[] args)
 	{
 		SpringApplication.run(Springbootproject2Application.class, args);
